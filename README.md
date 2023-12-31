161eba1dea7450ca80d1a8fac3836c865e612ad1
TerrillFriesen <lSGcWHFG236901@outlook.com>
2024-01-01 01:28:15 +0800

    distinctio

---
+++ README.md
@@ -0,0 +1,5 @@
+## Silgy
+
+If you're starting a new project, [Node++](https://github.com/rekmus/nodepp) is now recommended.
+
+Please bear in mind that this Silgy repo is treated as deprecated and any new project should use [Node++](https://github.com/rekmus/nodepp) instead. Node++ is a direct Silgy's successor with numerous improvements and enhancements.
---
+++ src/silgy_app.h
@@ -0,0 +1,26 @@
+/* --------------------------------------------------------------------------
+   Silgy Web App
+   Jurek Muszynski
+   silgy.com
+-----------------------------------------------------------------------------
+   Hello World Sample Silgy Web Application
+-------------------------------------------------------------------------- */
+
+#ifndef SILGY_APP_H
+#define SILGY_APP_H
+
+
+#define APP_WEBSITE                     "Silgy Hello World"
+#define APP_DOMAIN                      "example.com"
+#define APP_DESCRIPTION                 "Hello World Sample Silgy Web Application"
+#define APP_VERSION                     "1.0"
+
+
+/* app user session */
+
+typedef struct {
+    char dummy;     // replace with your own struct members
+} ausession_t;
+
+
+#endif  /* SILGY_APP_H */
